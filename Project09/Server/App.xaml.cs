ede6b93740397943914c854eea3b5d72455ee976
kdy <kdoy990805@gmail.com>
2025-02-25 18:02:41 +0900

    서버 통신 작업중

---
+++ Project09/Server/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Server;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    static string accountDB = "Account.db";
+
+    // 내문서 경로 가져오는 방법
+    //string forderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+    // 현재 프로젝트 실행 경로 가져오는 방법 (프로젝트\bin\Debug\net8.0-windows)
+    //string forderPath = Environment.CurrentDirectory;
+
+    // 현재 프로젝트 경로 가져오기
+    static string projectPath = "../../../";
+    // 생성될 DB 경로(현재 프로젝트 경로 + 파일이름) 지정
+    public static string databasePath = System.IO.Path.Combine(projectPath, accountDB);
+}
+
---
+++ Project09/Server/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Server;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    Socket ServerSocket;
+    Socket ClientSocket;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void openServerButton_Click(object sender, RoutedEventArgs e)
+    {
+        ServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        var endPoint = new IPEndPoint(IPAddress.Any, Convert.ToInt32(portBox.Text));
+        ServerSocket.Bind(endPoint);
+        ServerSocket.Listen(10);
+
+    }    
+}
